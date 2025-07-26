1ea4f617e44476f1054dba195ed21552f11702e6
g4mm4p4nd4 <57543587+g4mm4p4nd4@users.noreply.github.com>
2025-07-26 15:27:09 -0700

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,11 @@
-mport { defineConfig } from '@playwright/test';
+import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-    webServer: {
+webServer: {
     command: 'npm run dev --prefix client',
     port: 3000,
     timeout: 120 * 1000,
     reuseExistingServer: true,
-  },
   },
   testDir: './tests/e2e',
   use: {
